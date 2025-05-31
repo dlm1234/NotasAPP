6a4fab03bd74bffdc741b1452db7bd5134c6c4f9
dlm1234 <154059424+dlm1234@users.noreply.github.com>
2025-05-31 15:19:04 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://simple-to-do-app-gmls.onrender.com';
 
 export const getTasks = async () => {
   const response = await fetch(`${API_URL}/tasks`);
